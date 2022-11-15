f9134dc566a0762664432340c830ca5e2c7c0118
DHenox <DHenox@github.com>
2022-11-15 20:27:30 +0100

    Ordenació dels nodes en la poda

--- src/edu/epsevg/prop/lab/c4/ProPlayer.java
+++ src/edu/epsevg/prop/lab/c4/ProPlayer.java
@@ -16,11 +16,12 @@ public class ProPlayer implements Jugador, IAuto {
     private int profunditat;
     private int myColor;
     private int cntNodes;
+    private int taulerSize;
     
-    /*  
-        Taula que associa a cada posició del tauler
-        el nº de possibles 4 en ratlla donat un color
-    */
+    /**
+     * Taula que associa a cada posició del tauler
+     * el nº de possibles 4 en ratlla que pot conformar
+     */
     public int[][] predictTable = {
         {3, 4, 5, 7, 7, 5, 4, 3},
         {4, 6, 8,10,10, 8, 6, 4},
@@ -34,7 +35,7 @@ public class ProPlayer implements Jugador, IAuto {
     
     /**
      * Constructor de ProPlayer parametritzat amb la profunditat maxima
-     * @param depth profunditat maxima
+     * @param depth profunditat maxima per a minimax
      */
     public ProPlayer(int depth)
     {
@@ -42,6 +43,7 @@ public class ProPlayer implements Jugador, IAuto {
         profunditat = depth;
         myColor = 0;
         cntNodes = 0;
+        taulerSize = 0;
     }
 
     /**
@@ -49,11 +51,12 @@ public class ProPlayer implements Jugador, IAuto {
      * a partir de la situacio actual del tauler
      * @param t el tauler actualment
      * @param color el color del ProPlayer
-     * @return l'index de la columna
+     * @return l'index de la columna on tirar
      */
     public int moviment(Tauler t, int color)
     {
         myColor = color;
+        taulerSize = t.getMida();
         //  Tots els parametres d'aquesta funció estan explicats a la docu
         int col = minimax(t, profunditat, true, 0, Integer.MIN_VALUE, Integer.MAX_VALUE).snd;
         System.out.println("Nombre de jugades finals explorades: " + cntNodes);
@@ -69,10 +72,10 @@ public class ProPlayer implements Jugador, IAuto {
     public int heuristica(Tauler t){
         ++cntNodes;
         int value = 0;
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < taulerSize; i++) {
             if(isRowEmpty(t, i))
                 break;
-            for (int j = 0; j < 8; j++) {
+            for (int j = 0; j < taulerSize; j++) {
                 if (t.getColor(i, j) == myColor) {
                     value += predictTable[i][j];
                 } else if (t.getColor(i, j) == myColor*-1) {
@@ -87,7 +90,7 @@ public class ProPlayer implements Jugador, IAuto {
      * Funció que comprova si la fila indicada està totalment buida
      * @param t el tauler a analitzar
      * @param i la fila del tauler
-     * @return true si la fila i-éssima es buida, false altrament
+     * @return true si la fila i-éssima del tauler es buida, false altrament
      */
     public boolean isRowEmpty(Tauler t, int i){
         return (t.getColor(i, 0)==0 && t.getColor(i,1)==0 &&
@@ -109,18 +112,17 @@ public class ProPlayer implements Jugador, IAuto {
      */
     public Pair<Integer, Integer> minimax(Tauler t, int depth, boolean maximizingPlayer, int ultCol, int alpha, int beta){
         
-        if(t.solucio(ultCol, myColor))              //  Guanyem nosaltres
+        if(!maximizingPlayer && t.solucio(ultCol, myColor))             //  Guanyem nosaltres
             return new Pair<>(10000, ultCol);
-        else if(t.solucio(ultCol, myColor*-1))      //  Guanya el contrincant
+        else if(maximizingPlayer && t.solucio(ultCol, myColor*-1))      //  Guanya el contrincant
             return new Pair<>(-10000, ultCol);
-        else if(depth == 0 || !t.espotmoure()){     //  Empat o profunditat maxima
+        else if(depth == 0 || !t.espotmoure()){                         //  Empat o profunditat maxima
             return new Pair<>(heuristica(t), 0);
         }
-        
         int col = 0;
         if(maximizingPlayer){
             int maxEval = Integer.MIN_VALUE;
-            for (int i = 0; i < 8; i++) {
+            for (int i = 2; i != -1;) {
                 //  Si es poden afegir fitxes a la columna
                 if(t.movpossible(i)){
                     //  Copiem tot l'estat actual en un nou tauler
@@ -128,20 +130,21 @@ public class ProPlayer implements Jugador, IAuto {
                     //  Afegim una fitxa a una columna del nou tauler
                     f.afegeix(i, myColor);
                     int eval = minimax(f, depth-1, !maximizingPlayer, i, alpha, beta).fst;
-                    alpha = Math.max(maxEval, alpha);
                     if(maxEval < eval){
                         maxEval = eval;
                         col = i;
                     }
+                    alpha = Math.max(maxEval, alpha);
                     if(alpha >= beta)
                         break;
                 }
+                i = ordreNode(i);
             }
             return new Pair<>(maxEval, col);
         }
         else{
             int minEval = Integer.MAX_VALUE;
-            for (int i = 0; i < 8; i++) {
+            for (int i = 2; i != -1;) {
                 //  Si es poden afegir fitxes a la columna
                 if(t.movpossible(i)){
                     //  Copiem tot l'estat actual en un nou tauler
@@ -149,17 +152,47 @@ public class ProPlayer implements Jugador, IAuto {
                     //  Afegim una fitxa a una columna del nou tauler
                     f.afegeix(i, myColor*-1);
                     int eval = minimax(f, depth-1, !maximizingPlayer, i, alpha, beta).fst;
-                    beta = Math.min(minEval, beta);
                     if(minEval > eval){
                         minEval = eval;
                         col = i;
                     }
+                    beta = Math.min(minEval, beta);
                     if(alpha >= beta)
                         break;
                 }
+                i = ordreNode(i);
             }
             return new Pair<>(minEval, col);
         }
+    }
+    
+    /**
+     * Funció que determina l'ordenació dels nodes per
+     * a una poda encara mes optima
+     * @param i index de la columna anterior
+     * @return retorna la columna seguent
+     */
+    public int ordreNode(int i){
+        int res = 0;
+        
+        if(i == 2)
+            res = 3;
+        else if(i == 3)
+            res = 4;
+        else if(i == 4)
+            res = 5;
+        else if(i == 5)
+            res = 1;
+        else if(i == 1)
+            res = 6;
+        else if(i == 6)
+            res = 0;
+        else if(i == 0)
+            res = 7;
+        else if(i == 7)
+            res = -1;
+        
+        return res;
     }
     
     /**
